291fa5a0198e3c9ae41dbc87666aeceec1a4539d
Aiden Hakimoff <88609829+aidenhakimoff@users.noreply.github.com>
2023-07-19 12:29:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,7 +435,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons = [[
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TitanBotUpdates'),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/DVDUpdatesIN'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
