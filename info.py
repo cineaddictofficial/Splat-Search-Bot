c593a023ffce2ac740a85079a5426ecb3d4912cc
cineaddictofficial <cineaddictofficial@gmail.com>
2025-12-13 01:27:14 +0530

    Update info.py
    Bot is available to everyone.

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ PICS = (environ.get('PICS', 'https://te.legra.ph/file/84fcfcb9eaa399d702434.jpg'
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+AUTH_USERS = []   # empty list = allow everyone
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
